0a397fed38462c1ab54a41d1194881c1fae63239
Asad Ali <itsasadali5@gmail.com>
2025-10-23 23:45:14 +0500

    Enhance patterns.cpp with additional commented patterns and modify triangle pattern logic

--- patterns.cpp
+++ patterns.cpp
@@ -36,12 +36,125 @@ int main()
 
     // -----------------------------------
 
-    int n = 4;
-    char ch = 'A';
+    // int n = 5;
+    // char ch = 'A';
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 0; j < n; j++) {
+    //         cout << ch << " ";
+    //         ch = ch + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // TRIANGLE PATTERNS
+
+    // int n = 6;
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < i + 1; j++){
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // int n = 4;
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 0; j < i + 1; j++) {
+    //         cout << i + 1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // int n = 5;
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < i + 1; j++){
+    //         cout << ch << " ";
+    //     }
+    //     cout << endl;
+    //     ch++;
+    // }
+
+    // -----------------------------------
+
+    // int n = 5;
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = 1; j <= i + 1 ; j++) {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // int n = 6;
+    // for(int i = 0; i < n; i++) {
+    //     for(int j = i + 1; j > 0; j--) {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // FLOYD'S TRIANGLE PATTERN
+
+    // int n = 7;
+    // int count = 1;
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < i + 1 ; j++){
+    //         cout << count++ << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // int n = 7;
+    // int count = 1;
+    // for (int i = 0; i < n; i++){
+    //     for (int j = i + 1; j > 0 + 1 ; j--){
+    //         cout << count++ << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    // int n = 5;
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < i + 1 ; j++){
+    //         cout << ch << " ";
+    //         ch = ch + 1;
+    //     }
+    //     cout << endl;
+    // }
+
+    // -----------------------------------
+
+    int n = 4; // inprogress
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            cout << ch << " ";
-            ch = ch + 1;
+        for(int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for(int k = 0; k <= i; k++) {
+            cout << i ;
         }
         cout << endl;
     }
