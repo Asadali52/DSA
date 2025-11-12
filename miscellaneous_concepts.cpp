d4f7d29dfddc618028c723201f3cc6e43db1fa16
Asad Ali <itsasadali5@gmail.com>
2025-11-12 23:10:58 +0500

    Refactor miscellaneous_concepts.cpp to comment out bitwise operation examples and retain shift operation demonstrations

--- miscellaneous_concepts.cpp
+++ miscellaneous_concepts.cpp
@@ -4,11 +4,20 @@ using namespace std;
 int main()
 {
 
-    int a = 8, b = 8;
-    cout << (a & b) << endl;
-    cout << (a | b) << endl;
-    cout << (a ^ b) << endl;
+    // int a = 8, b = 8;
+    // cout << (a & b) << endl;
+    // cout << (a | b) << endl;
+    // cout << (a ^ b) << endl;
     
+    // int x = 8 , y = 12;
+    // cout << (x & y) << endl;
+    // cout << (x | y) << endl;
+    // cout << (x ^ y) << endl;
+
+    cout << (10 << 2) << endl;
+    cout << (4 << 1) << endl;
+    cout << (10 >> 2) << endl;
+    cout << (8 >> 2) << endl;
 
     return 0;
 }
